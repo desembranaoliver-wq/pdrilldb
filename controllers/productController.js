74a83ad25becd1f7fa87b2c8b8db69b8ff4f970a
desembranaoliver-wq <desembranaoliver@gmail.com>
2026-03-06 15:03:39 +0800

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,7 +2,7 @@
 const connection = require("../config/db");
 //get all products
 exports.getAllGadgets = (req, res) => {
-  connection.query("SELECT * FROM students", (err, rows, fields) => {
+  connection.query("SELECT * FROM enrollment", (err, rows, fields) => {
     if (err) throw err;
     res.json(rows);
   });
@@ -11,7 +11,7 @@ exports.getAllGadgets = (req, res) => {
 exports.createProducts = (req, res) => {
   const {fullname, course, yearlevel, email, dateenrolled} = req.body;
   connection.query(
-    "INSERT INTO students (fullname, course, yearlevel, email, dateenrolled) VALUES (?, ?, ?, ?, ?, ?)",
+    "INSERT INTO enrollment (fullname, course, yearlevel, email, dateenrolled) VALUES (?, ?, ?, ?, ?, ?)",
     [fullname, course, yearlevel, email, dateenrolled],
     (err, result) => {
       if (err) throw err;
@@ -22,6 +22,7 @@ exports.createProducts = (req, res) => {
     },
   );
 };
+
 
 
 
