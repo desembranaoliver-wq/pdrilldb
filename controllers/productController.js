7977dd86beff4baf52a45dbbbc1eb3716562ef69
desembranaoliver-wq <desembranaoliver@gmail.com>
2026-03-06 14:51:29 +0800

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,7 +9,7 @@ exports.getAllGadgets = (req, res) => {
 };
 
 exports.createProducts = (req, res) => {
-  const {itemName, unitPrice, quantity, supplier } = req.body;
+  const {id, fullname, course, yearlevel, email, dateenrolled} = req.body;
   connection.query(
     "INSERT INTO students (id, fullname, course, yearlevel, email, dateenrolled) VALUES (?, ?, ?, ?, ?, ?)",
     [itemName, unitPrice, quantity, supplier],
@@ -22,6 +22,7 @@ exports.createProducts = (req, res) => {
     },
   );
 };
+
 
 
 
